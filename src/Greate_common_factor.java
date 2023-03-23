a59c60da94ddaf580c301f9b02623c0901ddece0
tuuyen09 <tu190901@gmail.com>
2023-03-23 14:26:00 +0700

    Greate_common_factor

--- src/Greate_common_factor.java
+++ src/Greate_common_factor.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Greate_common_factor {
+import java.util.Scanner;
+
+public class Greate_common_factor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhap so nguyen a:");
+        int a = scanner.nextInt();
+
+        System.out.println("Nhap so nguyen b:");
+        int b = scanner.nextInt();
+
+        if (a == 0 || b == 0) {
+            if (a == 0) {
+                System.out.println(Math.abs(b));
+            } else {
+                System.out.println(Math.abs(a));
+            }
+        } else {
+            while (a != b) {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            System.out.println("Greatest common factor: " + a);
+
+        }
+    }
+
+
 }
+
+
