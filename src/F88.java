a45ceb752b4cd6ef209ac246e0cf9461f455e052
tuuyen09 <tu190901@gmail.com>
2023-03-23 10:47:45 +0700

    F88

--- src/F88.java
+++ src/F88.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class F88 {
+import java.util.Scanner;
+
+public class F88 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhap so tien cho vay:");
+        double money = scanner.nextDouble();
+
+        System.out.println("Nhap so thang cho vay:");
+        int month = scanner.nextInt();
+
+        System.out.println("Nhap lai suat hang thang:");
+        float interestRate = scanner.nextFloat();
+
+        double interest = money * month * interestRate;
+        System.out.println("So tien lai nhan duoc: " + interest);
+    }
 }
